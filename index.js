c5150661659e8f38023e0f3341401c59f62fa12d
Brian <ballerbrian4@gmail.com>
2018-03-18 12:20:09 -0700

    added topic change command (non-event)

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 /*eslint-disable no-console*/
-var http = require("http");
-var fs = require("fs");
+var http = require("http"); //uses http
+var fs = require("fs"); //uses fs
 //var path = require("path");
+//calls extrack.js to start when index.js is run
 var extract = require("./extract");
+//mime to detect file extensions
 const mime = require("mime");
+//establish the websocketserver to initialize with the other modules when index.js runs
+var wss = require("./websockets-server");
 
-
+//if file path does not exist, then call handleError
 var handleError = function(err, res){
-  res.writeHead(404);
+  res.writeHead(404); //write error to browser
+  //read the 404 image from app/error.html
   fs.readFile("app/error.html", function(err, data){
     res.end(data);
   });
@@ -33,7 +38,9 @@ var server = http.createServer(function (req, res){
       handleError(err, res);
       return;
     } else {
+      //media type detected using mime.getType
     var MediaType = mime.getType(filePath);
+    //sets the header of the content type to whatever is detected
     res.setHeader("Content-Type", MediaType);
     res.end(data);
     }
